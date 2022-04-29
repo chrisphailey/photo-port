09599e9fa0f0c9b90f6ceb52ef9ef3d86cb0d5f5
chrisphailey <93353718+chrisphailey@users.noreply.github.com>
2022-04-28 20:08:36 -0500

    contact form tests

---
+++ src/components/Contact/__test__/index.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render, cleanup } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import ContactForm from "..";
+
+afterEach(cleanup);
+
+describe("Contact component", () => {
+  it("renders", () => {
+    render(<ContactForm></ContactForm>);
+  });
+});
